18937749267ccb615a70ed6b5f79a59de39d2709
cirilocodes <cirilolupin@gmail.com>
2025-08-13 01:08:45 +0000

    finally prototype one

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,17 +1,17 @@
-module.exports = function(api) {
+// babel.config.js
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
-      ['@react-native-voice/voice', { /* ... */ }],
-      ['expo-secure-store', { /* ... */ }],
+      'nativewind/babel',
       ['module:react-native-dotenv', {
         moduleName: '@env',
         path: '.env',
         safe: false,
         allowUndefined: true,
-      }]
+      }],
+      'react-native-reanimated/plugin',
     ],
   };
 };
